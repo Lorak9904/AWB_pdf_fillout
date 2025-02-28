532ca6c5be1a0472f62321f1a499ad3b80aaec78
bobbyskywalker <aleksander.garbacz@gmail.com>
2025-02-28 18:09:36 +0100

    pdf populator class refactored

---
+++ generate_pdf/populate_pdf.py
@@ -0,0 +1,68 @@
+import json
+import fitz
+from text_positions import TextPositions
+
+# TODO:
+# scaling, fonts, length optimization
+# unit tests
+
+class PDFPopulator:
+    def __init__(self, template_path: str, output_pdf_path: str, json_path: str) -> None:
+        self.template_path: str = template_path
+        self.output_pdf_path: str = output_pdf_path
+        self.json_path: str = json_path
+        self.text_positions = TextPositions().text_positions
+        self.fontsize = 6
+        self.color = (0, 0, 0)
+    
+    def load_data(self) -> dict:
+        with open(self.json_path, "r") as f:
+            return json.load(f)
+
+    def fill_pdf(self) -> None:
+        awb_data = self.load_data()
+        doc = fitz.open(self.template_path)
+        page = doc[0]
+
+        # shipper data
+        page.insert_text(self.text_positions["AWB_Number"], awb_data["AWB_Number"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Shipper_Name"], awb_data["Shipper"]["Name"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Shipper_Address"], awb_data["Shipper"]["Address"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Shipper_Phone"], awb_data["Shipper"]["Phone"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Shipper_Account_Number"], awb_data["Shipper"]["Account_Number"], fontsize=self.fontsize, color=self.color)
+        # consignee data
+        page.insert_text(self.text_positions["Consignee_Name"], awb_data["Consignee"]["Name"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Consignee_Address"], awb_data["Consignee"]["Address"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Consignee_Phone"], awb_data["Consignee"]["Phone"], fontsize=self.fontsize, color=self.color)
+        # issuing carrier data
+        page.insert_text(self.text_positions["Issuing_Carrier_Name"], awb_data["Issuing_Carrier"]["Name"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Issuing_Carrier_Address"], awb_data["Issuing_Carrier"]["Address"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Issuing_Carrier_Phone"], awb_data["Issuing_Carrier"]["Phone"], fontsize=self.fontsize, color=self.color)
+        # misc
+        page.insert_text(self.text_positions["Agent_IATA_Code"], awb_data["Agent_IATA_Code"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Agent_Account_No"], awb_data["Agent_Account_No"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Accounting_Information"], awb_data["Accounting_Information"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Airport_Departure"], awb_data["Airport_Departure"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Airport_Destination"], awb_data["Airport_Destination"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Flight_Data"], awb_data["Flight_Data"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Currency"], awb_data["Currency"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["WT/VAL/PPD"], awb_data["WT/VAL"]["PPD"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["WT/VAL/COLL"], awb_data["WT/VAL"]["COLL"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["OTHER/PPD"], awb_data["OTHER"]["PPD"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["OTHER/COLL"], awb_data["OTHER"]["COLL"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["CHGS"], awb_data["CHGS"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Reference_Number"], awb_data["Reference_Number"], fontsize=self.fontsize, color=self.color)
+        # carriage
+        page.insert_text(self.text_positions["Declared_Value_For_Carriage"], awb_data["Declared_Value_For_Carriage"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Declared_Value_For_Customs"], awb_data["Declared_Value_For_Customs"], fontsize=self.fontsize, color=self.color)
+        page.insert_text(self.text_positions["Handling_Information"], awb_data["Handling_Information"], fontsize=self.fontsize, color=self.color)
+        # save
+        doc.save(output_pdf_path)
+        doc.close()
+
+# example usage
+template_path = "../awb_templates/awb_template.pdf"
+output_pdf_path = "filled_awb.pdf"
+json_path = "../awb_templates/awb_example.json"
+filler = PDFPopulator(template_path, output_pdf_path, json_path)
+filler.fill_pdf()
---
+++ generate_pdf/text_positions.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+
+# text positions hardcoded to according to the template (awb_templates/awb_template.py)
+@dataclass
+class TextPositions:
+    text_positions = {
+    "AWB_Number": (70, 20),
+    "Shipper_Name": (65, 60),
+    "Shipper_Address": (65, 70),
+    "Shipper_Phone": (65, 80),
+    "Shipper_Account_Number": (225, 50),
+    "Consignee_Name": (65, 130),
+    "Consignee_Address": (65, 140),
+    "Consignee_Phone": (65, 150),
+    "Issuing_Carrier_Name": (65, 195),
+    "Issuing_Carrier_Address": (65, 205),
+    "Issuing_Carrier_Phone": (65, 215),
+    "Agent_IATA_Code": (65, 240),
+    "Agent_Account_No": (200, 240),
+    "Airport_Departure": (65, 265),
+    "Airport_Destination": (65, 310),
+    "Flight_Data": (200, 310),
+    "Reference_Number": (320, 265),
+    "Currency": (320, 285),
+    "CHGS": (350, 290),
+    "WT/VAL/PPD": (365, 290),
+    "WT/VAL/COLL": (380, 290),
+    "OTHER/PPD": (395, 290),
+    "OTHER/COLL": (410, 290),
+    "Accounting_Information": (350, 200),
+    "Declared_Value_For_Carriage": (425, 290),
+    "Declared_Value_For_Customs": (510, 290),
+    "Handling_Information": (60, 335),
+    # TODO: shipment data?
+}
